d4441f18ad74e05f8a3cbaf29d238d9447bfd6ab
RohanBhanderi <rohan.bhanderi@sjsu.edu>
2015-04-21 10:46:54 -0700

    Base code for project

---
+++ controllers/client.js
@@ -0,0 +1,106 @@
+var dateutil = require('../util/dateutil'),
+	moment = require('moment');
+
+createClient = function(req,res){
+	console.log(JSON.stringify(req.body));
+	if(!req.body.idperson || !req.body.start_date || !req.body.end_date || !req.body.idclient){
+		res.status(400).json({status : 400, message : "Bad Request"});
+	}else{
+		var formDate = moment(req.body.start_date,'DD-MM-YYYY').toDate();
+		var toDate = moment(req.body.end_date,'DD-MM-YYYY').toDate();
+
+		var queryParam = {
+				idperson : req.body.idperson,
+				start_date : formDate,
+				end_date : toDate,
+				idclient : req.body.idclient
+		}
+
+		mysql.queryDb("INSERT INTO client SET ?", queryParam, function(err, response) {
+			if (err) {
+				console.log("Error while perfoming query !!!");
+				res.status(500).json({ status : 500, message : "Please try again later" });
+			} else {
+				res.status(200).json({ status : 200, message : "Client has been added Succesfully" });
+			}
+		});
+	}
+};
+
+updateClient = function(req,res){
+	if(!req.body.idperson || !req.body.start_date || !req.body.end_date){
+		res.status(400).json({ status : 400, message : "Bad Request" });
+	}else{
+		var newParam ={
+				start_date : moment(req.body.start_date,'DD-MM-YYYY').toDate(),
+				end_date : moment(req.body.end_date,'DD-MM-YYYY').toDate(),
+		};
+		//and ?? = ? and ?? = ?
+		//'start_date',old.start_date,'end_date',old.end_date
+		mysql.queryDb("UPDATE client SET ? WHERE ?? = ?", 
+			[newParam,'idperson',req.body.idperson], 
+			function(err, response) {
+			if (err) {
+				console.log("Error while perfoming query !!!" + err);
+				res.status(500).json({ status : 500, message : "Please try again later" });
+			} else {
+				res.status(200).json({ status : 200, message : "Client has been updated Succesfully" });
+			}
+		});
+	}
+};
+
+getClient=function(req,res){
+	
+	if(!req.params.idperson){
+		res.status(400).json({ status : 400, message : "Bad Request" });
+	}else{
+		mysql.queryDb('SELECT * FROM client WHERE ?',[{idperson:req.params.idperson}],function(err,rows){
+
+			if (err) {
+				res.status(500).json({ status : 500, message : "Error while retrieving data" });
+			} else {
+				res.status(200).json({ status : 200, data : rows });
+			}
+		});
+	}
+};
+
+deleteClient=function(req,res){
+	console.log(JSON.stringify(req.body));
+	if(!req.body.idperson){
+		res.status(400).json({ status : 400, message : "Bad Request" });
+	}else{
+		var idperson = req.body.idperson,
+		idclient = req.body.idclient,
+		start_date = req.body.start_date,
+		end_date = req.body.end_date;
+
+		mysql.queryDb('DELETE FROM client WHERE ?',[{idperson:idperson}],function(err,response){
+			if (err) {
+				console.log("Error while deleting client details !!!");
+				console.log(err);
+				res.status(500).json({ status : 500, message : "Error while deleting client details !!!" });
+			} else {
+				res.status(200).json({ status : 200, message : "Client details has been deleted Succesfully" });
+			}
+		});
+	}
+};
+
+listAllClients=function(req,res){
+	mysql.queryDb('SELECT * FROM client',function(err,rows){
+		if (err) {
+			console.log("Error while listing all the client details !!!"  + err);
+			res.status(500).json({ status : 500, message : "Error while listing client details !!!" });
+		} else {
+			res.status(200).json({ status : 200, data : rows});
+		}
+	});
+};
+
+exports.createClient = createClient;
+exports.updateClient = updateClient;
+exports.deleteClient = deleteClient;
+exports.getClient = getClient;
+exports.listAllClients = listAllClients;
---
+++ routes.js
@@ -0,0 +1,36 @@
+var loginController = require('./controllers/login');
+var clientController = require('./controllers/client');
+
+module.exports = function (app, passport) {
+
+    // Home
+    app.get('/', function(req,res){ res.render("index"); });
+    app.get(['/home','/logout'], ensureAuthenticated, function(req,res){ res.render("index"); });
+
+    // Auth
+    app.post('/api/register', loginController.register);
+    app.get('/api/login', function(req,res){ res.render("index"); });
+    app.post('/api/login', loginController.checkLogin);
+    app.get('/api/loggedin',loginController.loggedin);
+    app.post('/api/logout', loginController.logout);
+    
+    // Clint
+    app.get('/api/getClient/:idperson', ensureAuthenticated, clientController.getClient);
+    app.get('/api/listAllClients', ensureAuthenticated, clientController.listAllClients);
+    app.put('/api/updateClient', ensureAuthenticated, clientController.updateClient);
+    app.post('/api/createClient', ensureAuthenticated, clientController.createClient);
+    app.delete('/api/deleteClient', ensureAuthenticated, clientController.deleteClient);
+   
+    //Elastick beanstalk healthcheck
+    app.get('/health',function(req,res){ res.send(200); });
+    
+    //Auth Middleware
+    function ensureAuthenticated(req, res, next) {
+        if (req.isAuthenticated()) { 
+            return next(); 
+        } else {
+            //res.redirect('/login');
+           // res.status(401).json({message : "Unauthorized access !!"}); 
+        }
+    }
+};
