88abeaa54955e518a2cec9e5ea2778b5c757c4b6
komalkishorvohra <komalkvohra@gmail.com>
2015-04-24 01:20:52 -0700

    list and delete guard in admin module

--- public/javascripts/utility/constants.js
+++ public/javascripts/utility/constants.js
@@ -30,7 +30,10 @@ var urlConstants = {
 	"REJECT_CONNECTION_REQUEST" : "/api/user/connection/reject",
 	"GET_ALL_CONNECTIONS" : "/api/user/connection/",
 	"DELETE_EDUCATION" : "/api/user/education/",
-	"DELETE_EMPLOYMENT" : "/api/user/experience/"
+	"DELETE_EMPLOYMENT" : "/api/user/experience/",
+	//komal
+	"GET_ALL_GUARDS" : "/api/listAllGuards",
+	"DELETE_GUARD"	:	"/api/deleteGuard"
 };
 
 /**
