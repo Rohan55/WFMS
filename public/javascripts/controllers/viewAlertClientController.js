773edac746506cc7ccc8d67551dd5597adcdb7f3
rishabh7257 <rishabh7257@gmail.com>
2015-04-24 19:36:42 -0700

    Client

--- public/javascripts/controllers/viewAlertClientController.js
+++ public/javascripts/controllers/viewAlertClientController.js
@@ -2,44 +2,34 @@
 wfms.controller("viewAlertClientController", function($scope, $rootScope,
 		$location, $window, DataService) {
 
-	
-	// $scope.template = "templates/client/clientHome.html";
-
-	// $scope.setTemplate = function(tabName){
-	// 	$scope.template = "templates/client/"+tabName + ".html";
-	// }
-
-	// $scope.getTemplate = function(){
-	// 	return $scope.template;
-	// };
-	
-
 	$scope.getAlert = function(){
 		DataService.getData("/api/alertPerClient/1", []).success(
 				function(response) {
-
-					console.log("Hi"+response.resultAlert);
 					$scope.alert = response.resultAlert;
 				}).error(function(err) {
 			console.log("Error while fetching data");
 		});
 	};
 
-	$scope.seen = function(idalertInfo){
-		console.log("Id Alert: "+ this.idalertInfo);
+	$scope.seen = function(alertinfo){
+		console.log(angular.isObject(alertinfo));
+		angular.toJson(alertinfo);
+		console.log(alertinfo.data.idalertInfo);
+		console.log("Id Alert: "+ alertinfo);
 		var params = {
-				idalertInfo : this.idalertInfo,
+				idalertInfo : alertinfo.data.idalertInfo,
 				seenByClient : 'T'
 				
 			};
 
 		DataService.putData("/api/alert/seenByClient",params).success(function(response){
-				console.log("Failed");
-				console.log("Failed");
+				console.log("Done Successfully");
+
 
 			}).error(function(err){
-				console.log("Done");
+				console.log("Error");
 			});
+			this.getAlert();
 		
 	}
 	// $scope.signInFormError = "";
--- routes.js
+++ routes.js
@@ -50,15 +50,11 @@ module.exports = function (app, passport) {
     app.get('/api/alertPerBuilding/:idbuilding', ensureAuthenticated, alertController.alertPerBuilding);
     app.get('/api/alertPerClient/:idclient', ensureAuthenticated, alertController.alertPerClient);
     app.get('/api/alertPerDay/:date', ensureAuthenticated, alertController.alertPerDay);
-<<<<<<< HEAD
+
     app.put('/api/alert/seenByClient', ensureAuthenticated, alertController.seenByClient);
     
-   
-=======
-    app.get('/api/activeAdminAlerts', ensureAuthenticated, alertController.activeAdminAlerts);
-    
->>>>>>> 171855c92b80d7d86cafc2d6ea844cfa2cdb5ada
 
+    app.get('/api/activeAdminAlerts', ensureAuthenticated, alertController.activeAdminAlerts);
    
 
     
@@ -73,6 +69,7 @@ module.exports = function (app, passport) {
 
     
     //Building
+    app.get(('/api/getBuildingClientReport/:idperson', buildingController.getBuildingClientReport);
     app.get('/api/listBuilding/:idperson', buildingController.getBuilding);
     app.post('/api/createBuilding', buildingController.createBuilding);
     app.put('/api/editBuilding/:buildingid', buildingController.editBuilding);
